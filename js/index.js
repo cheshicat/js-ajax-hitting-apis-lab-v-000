c61c5ea7d486ae1018a132d6056453b36bef80aa
Allison Howard <alliemhow@gmail.com>
2019-09-12 01:36:07 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function getRepositories() {
 }
 
 function showRepositories() {
-  var repos = JSON.parse(this.responseText);
+  const repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos
     .map(
