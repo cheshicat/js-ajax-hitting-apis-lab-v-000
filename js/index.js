381f8aeca46c247c3e7a5aaac05f3715685c906a
Allison Howard <alliemhow@gmail.com>
2019-09-12 01:54:51 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -27,6 +27,6 @@ function getCommits(repo) {
   const name = repo.dataset.repository;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.open('GET', 'https://api.github.com/repos/' + owner + '/' + name + '/commits');
   req.send();
 }
