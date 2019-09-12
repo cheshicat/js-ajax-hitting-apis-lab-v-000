c0895380af4dda59645cc111168163ceda376913
Allison Howard <alliemhow@gmail.com>
2019-09-12 02:32:14 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function getRepositories() {
   let username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', `https://api.github.com/users/${username}/repos`);
   req.send();
 }
